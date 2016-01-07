75416256846a8e20446dcf88e1dccb0efb6d383f
Babouchot <bastien.maureille@hotmail.fr>
2016-01-07 10:04:51 +0100

    modified AppDelegate for new engine support

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -73,7 +73,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     }
     // if the frame's height is smaller than the height of medium size.
     else
-    {        
+    {
         director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
     }
 
