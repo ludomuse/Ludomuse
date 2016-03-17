89b3f3879c401822fc7b8e55daf673207c678c95
Babouchot <bastien.maureille@hotmail.fr>
2016-03-17 17:55:19 +0100

    [FIX] fixed crash under android when using an EditBox

--- Classes/Engine/Source/CEditBoxNode.cpp
+++ Classes/Engine/Source/CEditBoxNode.cpp
@@ -14,9 +14,8 @@ CEditBoxNode::CEditBoxNode(EAnchor a_eAnchor,
                            int a_iHeight,
                            int a_iXPosition,
                            int a_iYPosition) :
-    CEntityNode(a_eAnchor, a_iWidth, a_iHeight, a_iXPosition, a_iYPosition, m_sID)
+    CEntityNode(a_eAnchor, a_iWidth, a_iHeight, a_iXPosition, a_iYPosition)
 {
-  
 }
 
 
@@ -29,6 +28,12 @@ void CEditBoxNode::Init()
                                           ui::Scale9Sprite::create("Ludomuse/Content/textfieldBackground.png"));
   pBox->setPlaceHolder("Nom de joueur");
 
+  pBox->setFontSize(24);
+  pBox->setFontName("fonts/Open_Sans/OpenSans-Bold.ttf");
+  pBox->setFontColor(Color3B::BLACK);
+  pBox->setMaxLength(80);
+  pBox->setReturnType(ui::EditBox::KeyboardReturnType::DONE);
+  pBox->setInputMode(ui::EditBox::InputMode::SINGLE_LINE);
 
   m_pCocosEntity = pBox;
   
