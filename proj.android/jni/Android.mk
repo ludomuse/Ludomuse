beca17b07c4cc4200c6900ed038f9e160ea39858
Babouchot <bastien.maureille@hotmail.fr>
2016-03-17 17:03:07 +0100

    [ADD] added CEditBoxNode in Android.mk

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -28,6 +28,7 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/Engine/Source/CLabelNode.cpp \
                    ../../Classes/Engine/Source/CSpriteNode.cpp \
                    ../../Classes/Engine/Source/CMenuNode.cpp \
+                   ../../Classes/Engine/Source/CEditBoxNode.cpp \
                    ../../Classes/Engine/Source/CValidator.cpp \
                    ../../Classes/Engine/Source/CTransitionVisitor.cpp \
                    ../../Classes/Engine/Source/CTouchBeganVisitor.cpp \
