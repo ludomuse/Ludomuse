20c9299393beee3a3133d6315b22f05099caa87e
Babouchot <bastien.maureille@hotmail.fr>
2016-05-12 15:07:17 +0200

    [DEL] removed a regression that was causing incomplete transitions

--- Classes/Engine/Source/CTransitionVisitor.cpp
+++ Classes/Engine/Source/CTransitionVisitor.cpp
@@ -86,9 +86,9 @@ void CTransitionVisitor::InitScene(CSceneNode* a_pSceneNode)
 	{
 		Director::getInstance()->replaceScene(TransitionSlideInL::create(0.5f, pNewScene));
 	}
-	CSceneNode* pOldScene = m_pKernel->m_pCurrentScene;
+	//CSceneNode* pOldScene = m_pKernel->m_pCurrentScene;
 	m_pKernel->m_pCurrentScene = a_pSceneNode;
-	pOldScene->UnInit();
+	//pOldScene->UnInit();
 
 	M_STATS->StartStats();
 }
