dd926edddf1a1e24e16bc260a0e330f7d59b21bf
Rigaud Vincent <poonidessoo@hotmail.fr>
2016-07-05 11:17:18 +0200

    [ADD] parsing InfoNode to json

--- Classes/Engine/Include/CInfoNode.cpp
+++ Classes/Engine/Include/CInfoNode.cpp
@@ -16,11 +16,29 @@ CInfoNode::CInfoNode(const std::string &a_rFilename, EAnchor a_eAnchor, int a_iW
 
 void CInfoNode::ToJson(rapidjson::Value& a_rParent, rapidjson::Document::AllocatorType& a_rAllocator)
 {
-    // To do
     qDebug("Parsing CInfoNode");
     rapidjson::Value info(rapidjson::kObjectType);
     info.AddMember("type", "Info", a_rAllocator);
 
+    rapidjson::Value params(rapidjson::kObjectType);
+    params.AddMember("source", rapidjson::Value(m_sSpriteFilename.c_str(), m_sSpriteFilename.length()), a_rAllocator);
+    params.AddMember("anchor", m_eAnchor, a_rAllocator);
+    params.AddMember("width", m_iWidth, a_rAllocator);
+    params.AddMember("height", m_iHeight, a_rAllocator);
+
+    rapidjson::Value children(rapidjson::kArrayType);
+    if(!this->m_vChildren.empty())
+    {
+        rapidjson::Value children(rapidjson::kArrayType);
+        for(CNode* currentNode : this->m_vChildren)
+        {
+            currentNode->ToJson(children, a_rAllocator);
+        }
+        params.AddMember("children", children, a_rAllocator);
+    }
+
+    info.AddMember("params", params, a_rAllocator);
     a_rParent.PushBack(info, a_rAllocator);
 }
-}
+
+} // end namespace LM
