f4264e875078dd629c47abcf208d5a6958a59e02
ihmtek <bastien.maureille@hotmail.fr>
2016-07-29 15:55:24 +0200

    [ADD] added CNetworkManager for mac (same as the one on linux)

--- Classes/Modules/Networking/osx/Include/CNetworkManager.h
+++ Classes/Modules/Networking/osx/Include/CNetworkManager.h
@@ -4,57 +4,38 @@
 #include "../../../../Engine/Include/CKernel.h"
 
 
+
 namespace LM
 {
 
 class CNetworkManager
 {
- private:
+
+ public:
   CKernel* m_pKernel;
+  // SOCKET ConnectSocket;
+  // SOCKET ClientSocket;
+  // SOCKET ListenSocket;
+
+  bool m_bIsServer;
+
+  // struct addrinfo *ptr;
+  // struct addrinfo *result;
 
  public:
 
-  CNetworkManager(CKernel* a_pKernel, bool a_bIsServer);
-
-  virtual void onGettingPeers(std::vector<std::string> a_vPeers);
-
-  virtual void onReceiving(std::string s);
-
-  void onReceivingMsg(bytes byteArray);
-
-  // virtual void onReceiving(int i);
-
-  // virtual void onReceiving(bool b);
-
-  // virtual void onReceiving(long l);
-
-  // virtual void onReceivingFile(std::string path);
-
-  // virtual void onReceiving(double d);
-
-  // virtual void onReceiving(float f);
-
-  // virtual void onReceiving(char c);
-
+  CNetworkManager(CKernel* a_pKernel, bool a_bIsServer = true);
+  ~CNetworkManager();
+  
+  void Send(const char* buff, size_t size);
   void Send(const std::string& s);
-
+  void Send(const bytes& b);
   void DiscoverPeers();
-
   void ConnectTo(const std::string& a_sDeviceName);
   
-  // void send(double d);
-
-  // void send(float f);
-
-  // void send(char c);
-
-  // void sendByte(byte b);
-
-  void Send(bytes bytes);
-
-
+  
 };
 
 } // namespace LM
 
-#endif // _CNETWORKMANAGER_H_
+#endif /* _CNETWORKMANAGER_H_ */
