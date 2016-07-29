b7506d8dd8561d520b78081e959aaafdc34bc04a
Babouchot <bastien.maureille@hotmail.fr>
2016-07-29 11:15:52 +0200

    merging cross platforms fixes into master

--- Classes/Modules/Networking/Networking.h
+++ Classes/Modules/Networking/Networking.h
@@ -3,9 +3,10 @@
 
 #ifdef TARGET_OS_MAC
 #include "./osx/Include/CNetworkManager.h"
-#elif defined __linux__
+#elif defined __ANDROID__
 #include "./android/Include/CNetworkManager.h"
-// #include "./linux/Include/CNetworkManager.h"
+#elif defined __linux__
+#include "./linux/Include/CNetworkManager.h"
 #elif defined _WIN32 | defined _WIN64
 #include "./win/Include/CNetworkManager.h"
 #else // android
