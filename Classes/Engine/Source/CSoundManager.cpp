c3f1d705560744bea562067af1ee80a6dd7abd53
Bastien Maureille <bastien.maureille@hotmail.fr>
2016-10-11 12:44:36 +0200

    [FIX] fixed threading issues in CSoundManager

--- Classes/Engine/Source/CSoundManager.cpp
+++ Classes/Engine/Source/CSoundManager.cpp
@@ -9,12 +9,15 @@
 namespace LM
 {
 
-	void* WaitSoundFinished(void* a_pSoundManager)
+	void* WaitSoundFinished(void* a_pSoundEndedObject)
 	{
 		CCLOG("[SOUND] start waitSoundFinished");
-		CSoundManager* pSoundManager = static_cast<CSoundManager*>(a_pSoundManager);
+		SSoundEndedObject* pSoundEndedObject = static_cast<SSoundEndedObject*>(a_pSoundEndedObject);
 		CCLOG("[SOUND] after cast");
-		std::string sSoundURL = pSoundManager->m_sPlayingSoundURL;
+		std::string sThreadSoundURL = pSoundEndedObject->sSoundURL;
+		CSoundManager* pSoundManager = pSoundEndedObject->pSoundManager;
+		delete pSoundEndedObject;
+
 		CCLOG("[SOUND] after getting string");
 		while (CocosDenshion::SimpleAudioEngine::getInstance()->isBackgroundMusicPlaying())
 		{
@@ -24,12 +27,17 @@ namespace LM
 			usleep(100000);
 #endif
 			CCLOG("[SOUND] in while");
+			if (pSoundManager->GetSoundURL() != sThreadSoundURL)
+			{
+				return NULL;
+			}
+
 		}
-		if (sSoundURL != "")
+		CCLOG("[SOUND] before EndSound");
+		if (pSoundManager->GetSoundURL() == sThreadSoundURL)
 		{
-			CCLOG("[SOUND] before EndSound");
-			pSoundManager->EndSound(sSoundURL);
-			pSoundManager->m_sPlayingSoundURL = "";
+			pSoundManager->EndSound(sThreadSoundURL);
+			pSoundManager->SetSoundURL("");
 		}
 		return NULL;
 	}
@@ -41,12 +49,21 @@ namespace LM
 
 	void CSoundManager::PlaySound(const std::string& a_rSoundURL)
 	{
+		m_oPlayingSoundMutex.lock();
 		m_sPlayingSoundURL = a_rSoundURL;
+
+		SSoundEndedObject* pSound = new SSoundEndedObject;
+		pSound->sSoundURL = m_sPlayingSoundURL;
+		pSound->pSoundManager = this;
+
 		CocosDenshion::SimpleAudioEngine::getInstance()->playBackgroundMusic(a_rSoundURL.c_str(), false);
-		//pthread_t thread;
-		//CCLOG("[SOUND] before creating thread");
-		//pthread_create(&thread, NULL, &WaitSoundFinished, this);
-		//CCLOG("[SOUND] after creating thread");
+		pthread_t thread;
+		CCLOG("[SOUND] before creating thread");
+		pthread_create(&thread, NULL, &WaitSoundFinished, pSound);
+		CCLOG("[SOUND] after creating thread");
+
+		m_oPlayingSoundMutex.unlock();
+
 	}
 
 
@@ -64,5 +81,21 @@ namespace LM
 	{
 		CCLOG("[SOUND] sound ended : %s", a_rSoundURL.c_str());
 	}
+
+	std::string CSoundManager::GetSoundURL()
+	{
+		m_oPlayingSoundMutex.lock();
+		std::string sSoundURL = m_sPlayingSoundURL;
+		m_oPlayingSoundMutex.unlock();
+		return sSoundURL;
+	}
+
+	void CSoundManager::SetSoundURL(const std::string& a_sNewSound)
+	{
+		m_oPlayingSoundMutex.lock();
+		m_sPlayingSoundURL = a_sNewSound;
+		m_oPlayingSoundMutex.unlock();
+	}
+
 
 } // namespace LM
