afd3f1d55253d1259a164006a3edfb849701a9f2
Gnufgnuf <etienvia@gmail.com>
2017-02-09 18:46:10 +0100

    [fix] parsing bug with macro

--- Classes/Engine/Include/CJsonParser_impl.h
+++ Classes/Engine/Include/CJsonParser_impl.h
@@ -172,7 +172,8 @@ inline void CJsonParser::ParseJson(RefJsonNode a_rJsonNode, T* a_pNode, bool a_b
 		std::string sFontName = "";
 		if (rParams.HasMember("source"))
 		{
-            sFontName =  m_sBasePath + rParams["source"].GetString();
+//            sFontName =  m_sBasePath + rParams["source"].GetString();
+            sFontName = NormalizePath(rParams["source"].GetString()	);
 		}
 		if (sFontName == "") // apply default font
 		{
