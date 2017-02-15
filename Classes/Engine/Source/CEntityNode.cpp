797f92a6f2c8956d4a363e0958a8aa90821cfeb2
Bastien Maureille <bastien.maureille@hotmail.fr>
2017-02-15 18:03:21 +0100

    [FIX] stopped all actions in CEntityNode::UnInit.
    Should fix segfaults on fade effects directly followed by transitions (segfaults occuring in cocos code)

--- Classes/Engine/Source/CEntityNode.cpp
+++ Classes/Engine/Source/CEntityNode.cpp
@@ -35,6 +35,8 @@ void CEntityNode::UnInit(bool removeChild)
 		if(removeChild)
 			GetParentScene()->removeChild(m_pCocosEntity);
 
+        m_pCocosEntity->stopAllActions();
+
 		m_pCocosEntity = nullptr;
 		CNode::UnInit(removeChild);
 	}
