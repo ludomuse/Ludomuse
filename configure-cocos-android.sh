d66791b9124a9619c602f8d08ecca25e1b50e718
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-22 16:53:15 +0100

    [TRAVIS] trying to set android sdk root for cocos

--- configure-cocos-android.sh
+++ configure-cocos-android.sh
@@ -7,6 +7,7 @@ cd $PREF
 wget --quiet https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
 unzip -q android-ndk-r13b-linux-x86_64.zip
 export NDK_ROOT=$PREF/android-ndk-r13b-linux-x86_64
+export ANDROID_SDK_ROOT=/usr/local/android-sdk/
 wget --quiet http://cdn.cocos2d-x.org/cocos2d-x-3.16.zip
 unzip -q cocos2d-x-3.16.zip
 cd cocos2d-x-3.16/
