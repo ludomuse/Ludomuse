443ee135c2a99f4c23c2e66c6287dcbe1fdea83a
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-22 17:00:13 +0100

    [TRAVIS] forgot force no on cocos new invocation for data usage question

--- configure-cocos-android.sh
+++ configure-cocos-android.sh
@@ -14,6 +14,6 @@ cd cocos2d-x-3.16/
 python2 setup.py
 cd $PREF
 export COCOS=$PREF/cocos2d-x-3.16/tools/cocos2d-console/bin/cocos
-$COCOS new LudoMuse -l cpp com.IHMTEK.LudoMuse
+yes N | $COCOS new LudoMuse -l cpp com.IHMTEK.LudoMuse
 cp -r Ludomuse/* LudoMuse
 cd LudoMuse
