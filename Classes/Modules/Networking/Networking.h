2d62de6f14fc7c9fd9aa3746510d84bd501c7c63
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-23 10:47:59 +0100

    [MODIF] merged mac and linux network files under unix and modofied CMakeLists and include files accordingly

--- Classes/Modules/Networking/Networking.h
+++ Classes/Modules/Networking/Networking.h
@@ -2,11 +2,11 @@
 #define _NETWORKING_H_
 
 #ifdef TARGET_OS_MAC
-#include "./linux/Include/CNetworkManager.h"
+#include "./unix/Include/CNetworkManager.h"
 #elif defined __ANDROID__
 #include "./android/Include/CNetworkManager.h"
 #elif defined __linux__
-#include "./linux/Include/CNetworkManager.h"
+#include "./unix/Include/CNetworkManager.h"
 #elif defined _WIN32 | defined _WIN64
 #include "./win/Include/CNetworkManager.h"
 #else // android
