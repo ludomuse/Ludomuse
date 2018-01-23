0b6c4db829410ab3918498ba03b2599fd9063f76
Babouchot <bastien.maureille@hotmail.fr>
2018-01-24 00:24:42 +0100

    [TRAVIS] trying to update android tools anyway

--- configure-cocos-android.sh
+++ configure-cocos-android.sh
@@ -18,7 +18,7 @@ if test ! -e $HOME/android-sdk-dl/sdk-tools.zip; then
     echo "unzipping tools"
     unzip -qq -n $HOME/android-sdk-dl/sdk-tools.zip -d $HOME/android-sdk
     echo "downloading build tools and sdk"
-    #echo y | $HOME/android-sdk/tools/bin/sdkmanager 'tools' > /dev/null
+    echo y | $HOME/android-sdk/tools/bin/sdkmanager 'tools;25.2.5' > /dev/null
     echo y | $HOME/android-sdk/tools/bin/sdkmanager 'platform-tools' > /dev/null
     echo y | $HOME/android-sdk/tools/bin/sdkmanager 'build-tools;23.0.1' > /dev/null
     echo y | $HOME/android-sdk/tools/bin/sdkmanager 'platforms;android-22' > /dev/null
