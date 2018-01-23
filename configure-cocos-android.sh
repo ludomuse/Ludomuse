c3ca7039ebc448280847480c8c9c47c75209241e
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-23 16:25:46 +0100

    [TRAVIS] trying to figure out android issues

--- configure-cocos-android.sh
+++ configure-cocos-android.sh
@@ -4,16 +4,43 @@
 echo "Before install path : $PWD"
 export PREF=$PWD/../
 cd $PREF
-wget --quiet https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
-unzip -q android-ndk-r13b-linux-x86_64.zip
-export NDK_ROOT=$PREF/android-ndk-r13b-linux-x86_64
-export ANDROID_SDK_ROOT=/usr/local/android-sdk/
-wget --quiet http://cdn.cocos2d-x.org/cocos2d-x-3.16.zip
-unzip -q cocos2d-x-3.16.zip
-cd cocos2d-x-3.16/
-python2 setup.py
+
+
+# setup sdk
+if test ! -e $HOME/android-sdk-dl/sdk-tools.zip; then
+    curl -s https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip > $HOME/android-sdk-dl/sdk-tools.zip ;
+    curl -s https://dl.google.com/android/repository/tools_r25.2.5-linux.zip > $HOME/android-sdk-dl/sdk-tools-25.zip ;
+fi
+
+unzip -qq -n $HOME/android-sdk-dl/sdk-tools.zip -d $HOME/android-sdk
+export ANDROID_SDK_ROOT=$HOME/android-sdk
+echo y | $HOME/android-sdk/tools/bin/sdkmanager 'tools' > /dev/null
+echo y | $HOME/android-sdk/tools/bin/sdkmanager 'platform-tools' > /dev/null
+echo y | $HOME/android-sdk/tools/bin/sdkmanager 'build-tools;23.0.1' > /dev/null
+echo y | $HOME/android-sdk/tools/bin/sdkmanager 'platforms;android-22' > /dev/null
+# echo y | $HOME/android-sdk/tools/bin/sdkmanager 'extras;google;m2repository' > /dev/null
+unzip -qq -n $HOME/android-sdk-dl/sdk-tools-25.zip -d $HOME/android-sdk
+
+
+
+# setup ndk
+if test ! -e $HOME/android-ndk-dl/android-ndk.zip; then
+    curl -s https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip > $HOME/android-ndk-dl/android-ndk.zip ;
+fi
+unzip -qq -n $HOME/android-ndk-dl/android-ndk.zip -d $HOME/android-ndk
+export NDK_ROOT=$HOME/android-ndk
+
+
+# setup cocos
+if test ! -e $HOME/cocos-dl/cocos.zip; then
+    curl -s http://cdn.cocos2d-x.org/cocos2d-x-3.16.zip > $HOME/cocos-dl/cocos.zip ;
+fi
+unzip -qq -n $HOME/cocos-dl/cocos.zip -d $HOME/cocos/
+python2 $HOME/cocos/setup.py
+export COCOS=$HOME/cocos/tools/cocos2d-console/bin/cocos
+
+
 cd $PREF
-export COCOS=$PREF/cocos2d-x-3.16/tools/cocos2d-console/bin/cocos
 yes N | $COCOS new LudoMuse -l cpp -p com.IHMTEK.LudoMuse
 cp -r Ludomuse/* LudoMuse
 cd LudoMuse
